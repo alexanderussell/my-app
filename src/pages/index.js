7d6e3b62a365748d6002687430625fd7aed8c600
Alex Russell <alex.russell@admins-MacBook-Pro-3.local>
2018-08-21 21:11:52 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to <br />design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+  </div>
+)
+
+export default IndexPage
